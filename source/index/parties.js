1bad5c432b667cf6ab584a1434db92db84f83347
Jaiveer Chadda <jaiveer.chadda@gmail.com>
2025-04-25 00:16:15 +0400

    colours now working (still have to fix the misnamed and missing constituencies)

--- source/index/parties.js
+++ source/index/parties.js
@@ -1,4 +1,23 @@
+const partyColours = {
+    Labour: "#e4003b",
+    Liberal_Democrat: "#FAA61A",
+    Conservative: "#009FE3",
+    Green: "#008066",
+    Scottish_National_Party: "#FFF95D",
+    Plaid_Cymru: "#3F8428",
+    Reform_UK: "#00bed6",
+    Social_Democratic_and_Labour_Party: "#e4003b",
+    Sinn_Fein: "#0C694F",
+    Democratic_Unionist_Party: "#D36A4B",
+    Alliance_Party: "#CDAF2C",
+    Independent: "#909090",
+    Speaker_of_the_House_of_Commons: "#000000",
+    Traditional_Unionist_Voice: "#0C3A6A",
+    Ulster_Unionist_Party: "#A1CDF0"
+}
+
 let data = {};
+let formattedData = []
 
 function findWinner(fullObject) {
     let maxName = null;
@@ -37,18 +56,27 @@ fetch('../../resources/constituency_info/results_formatted.csv')
         });
         console.log(data)
 
-        let formattedData = []
-
         for (let i=0;i<data.length;i++) {
             formattedData.push({
                 Name: data[i].Name,
                 ONS: data[i]["ONS Code"],
-                Winner: findWinner(data[i])
+                Winner: findWinner(data[i]).replace(/ /g, "_")
             })
         }
 
-        console.log(formattedData)
+        for (let i=0;i<formattedData.length;i++) {
+            let nameFormatted = formattedData[i].Name.replace(/ /g, "_");
 
+            try {
+                let constituency = document.getElementById(nameFormatted);
+                constituency.style.setProperty('--constituency_colour', partyColours[formattedData[i].Winner]);
+            } catch (error) {
+                // console.log(nameFormatted)
+                // console.error(error);
+            }
+        }
+
+        console.log(formattedData)
     }
 )
 .catch(
