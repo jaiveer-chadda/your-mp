1947cde4bb40988f6ec3f2fdae6262eaa1282cd8
Jaiveer Chadda <jaiveer.chadda@gmail.com>
2025-04-26 21:58:18 +0400

    added basic functionality for rest of infobox

--- source/index/infobox.js
+++ source/index/infobox.js
@@ -2,14 +2,22 @@ import { getConstituencyData } from '../master.js';
 
 const paths = document.querySelectorAll('#map_container svg path');
 const display = document.getElementById('infobox_title');
+const info = document.getElementById('infobox');
 
 paths.forEach(path => {
     path.addEventListener('mouseenter', evt => {
         display.textContent = evt.target.id.replace(/_/g, " ");
+        display.style.fontWeight = 'bold';
     });
     path.addEventListener('mouseleave', () => {
         display.textContent = 'Hover over a constituency';
+        display.style.fontWeight = 'normal';
+    });
+
+    path.addEventListener('click', evt => {
+        info.innerHTML = '';
+
     });
 });
 
-const formattedData = await getConstituencyData();
+// const formattedData = await getConstituencyData();
