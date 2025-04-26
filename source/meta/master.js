a91388f11bfb9c7e61feea16d6134b14e8ade198
Jaiveer Chadda <jaiveer.chadda@gmail.com>
2025-04-27 00:32:47 +0400

    moved party colours to master.js

--- source/meta/master.js
+++ source/meta/master.js
@@ -1,6 +1,26 @@
 export function addUS(text) { return text.replace(/ /g, "_"); }
 export function remUS(text) { return text.replace(/_/g, " "); }
 
+export function getPartyColours() {
+    return {
+        Labour: "#e4003b",
+        Liberal_Democrat: "#FAA61A",
+        Conservative: "#009FE3",
+        Green: "#008066",
+        Scottish_National_Party: "#FFF95D",
+        Plaid_Cymru: "#3F8428",
+        Reform_UK: "#00bed6",
+        Social_Democratic_and_Labour_Party: "#e4003b",
+        Sinn_Fein: "#0C694F",
+        Democratic_Unionist_Party: "#D36A4B",
+        Alliance_Party: "#CDAF2C",
+        Independent: "#909090",
+        Speaker_of_the_House_of_Commons: "#000000",
+        Traditional_Unionist_Voice: "#0C3A6A",
+        Ulster_Unionist_Party: "#A1CDF0"
+    }
+}
+
 function findWinner(fullObject) {
     let maxName = null;
     let maxValue = -Infinity;
--- source/meta/parties.js
+++ source/meta/parties.js
@@ -1,22 +1,4 @@
-import { getConstituencyWinners, addUS } from './master.js';
-
-const partyColours = {
-    Labour: "#e4003b",
-    Liberal_Democrat: "#FAA61A",
-    Conservative: "#009FE3",
-    Green: "#008066",
-    Scottish_National_Party: "#FFF95D",
-    Plaid_Cymru: "#3F8428",
-    Reform_UK: "#00bed6",
-    Social_Democratic_and_Labour_Party: "#e4003b",
-    Sinn_Fein: "#0C694F",
-    Democratic_Unionist_Party: "#D36A4B",
-    Alliance_Party: "#CDAF2C",
-    Independent: "#909090",
-    Speaker_of_the_House_of_Commons: "#000000",
-    Traditional_Unionist_Voice: "#0C3A6A",
-    Ulster_Unionist_Party: "#A1CDF0"
-}
+import { getConstituencyWinners, addUS, getPartyColours } from './master.js';
 
 const formattedData = await getConstituencyWinners();
 
@@ -25,7 +7,7 @@ for (let i=0;i<formattedData.length;i++) {
 
     try {
         let constituency = document.getElementById(nameFormatted);
-        constituency.style.setProperty('--constituency_colour', partyColours[formattedData[i].Winner]);
+        constituency.style.setProperty('--constituency_colour', getPartyColours()[formattedData[i].Winner]);
     } catch (error) {
         // console.log(nameFormatted)
         console.error(nameFormatted, error);
