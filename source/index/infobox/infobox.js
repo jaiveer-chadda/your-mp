54460c5f915f9ea608e9cf022e91e797484667a1
Jaiveer Chadda <jaiveer.chadda@gmail.com>
2025-04-28 18:53:39 +0400

    added basic image functionality
    need to set img placement and actually make it work now

--- source/index/infobox/infobox.js
+++ source/index/infobox/infobox.js
@@ -5,21 +5,22 @@ const constituencyWinners = await getConstituencyWinners();
 
 const paths = document.querySelectorAll('#map_container svg path');
 const infoboxTitle = document.getElementById('infobox_title');
+const MP_img = document.getElementById('infobox_mp_img');
 
-let infoLocked = false;
+let infoBoxLocked = false;
 
 paths.forEach(path => {
     path.addEventListener('mouseenter', evt => {
-        if (infoLocked) { return; }
+        if (infoBoxLocked) { return; }
         infoboxTitle.textContent = remUS(evt.target.id);
     });
     path.addEventListener('mouseleave', () => {
-        if (infoLocked) { return; }
+        if (infoBoxLocked) { return; }
         infoboxTitle.textContent = 'Hover over a constituency';
     });
 
     path.addEventListener('click', evt => {
-        infoLocked = true;
+        infoBoxLocked = true;
         const constituencyData = constituencyWinners.find(cons => cons.name === remUS(evt.target.id));
 
         document.querySelector("#infobox_title").textContent              = remUS(evt.target.id);
@@ -30,17 +31,19 @@ paths.forEach(path => {
         document.querySelector("#infobox_mp_name").textContent            = constituencyData.MP_name;
         document.querySelector("#infobox_mp_party").textContent           = remUS(constituencyData.winner_2024);
         document.querySelector("#infobox_mp_tenure").textContent          = '--tenure--';
+        MP_img.src = "resources/images/stand_in_mp_portrait.png"
+        MP_img.style.display = "inline";
     });
 });
 
 document.addEventListener('click', (evt) => {
     if (!(evt.target.matches('#map_container svg path, #infobox, #infobox *'))) {
-        infoLocked = false;
-
+        infoBoxLocked = false;
         infoboxTitle.textContent = 'Hover over a constituency';
 
         document.querySelectorAll("#infobox p:not(#infobox_title)").forEach(p => {
             p.textContent = '';
         });
+        MP_img.style.display = "none";
     }
 });
